a7504339dc4ff6e80120af74649ace039676a192
malek <malek.chtioui@esprit.tn>
2023-05-06 12:53:16 +0200

    malek

---
+++ src/utilities/Statics.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+/**
+ *
+ * @author achref
+ */
+public class Statics {
+    public static String BASE_URL = "http://127.0.0.1:8000/";
+}
